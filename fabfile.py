b6f0c7e64a7584184a1f38af76fdee3bda752d57
tcooc <tcooc@users.noreply.github.com>
2015-12-11 15:58:13 -0500

    Deploy.

--- fabfile.py
+++ fabfile.py
@@ -1,131 +1,79 @@
+import json
 from os import environ
 import re
 
-from fabric.api import local, runs_once, shell_env
+from fabric.api import hide, local, runs_once
 
+REFRESH_TOKEN_NAME = 'GOOGLE_REFRESH_TOKEN'
 
-ALL_ARGS = frozenset(['BOT_API_KEY', 'CI_COMMIT_ID', 'REFRESH_TOKEN'])
-# These are the same for prod and staging.
-COMMON_ARGS = frozenset(['BOT_API_KEY', 'CI_COMMIT_ID', 'REFRESH_TOKEN'])
-# These are different for prod and staging. Staging is prepended with "STAGING_"
-APPLICATION_ARGS = ALL_ARGS.difference(COMMON_ARGS)
-
-# Backend uses everything except REFRESH_TOKEN, and CI_COMMIT_ID.
-BACKEND_ARGS = ALL_ARGS.difference(['CI_COMMIT_ID', 'REFRESH_TOKEN'])
-
-
-app_config = {
-    'MODULES': 'app.yaml',
-    'CONFIGS': 'index.yaml queue.yaml cron.yaml',
-    'CONFIG': 'secrets.py'
-}
-
-
-def _overwrite(file_obj, text):
-    file_obj.seek(0)
-    file_obj.write(text)
-    file_obj.truncate()
+ALL_ARGS = frozenset(['BOT_USERNAME', 'BOT_API_KEY'])
 
 
 @runs_once
-def install_backend_dependencies():
+def install_xlib():
     local('pip install --upgrade --no-deps --requirement requirements_xlib.txt -t xlib')
     local('rm -rf xlib/*.egg-info xlib/*.dist-info xlib/VERSION xlib/*.pth')
 
 
 @runs_once
-def install_test_dependencies():
+def install():
     local('pip install --upgrade --quiet --requirement requirements.txt')
 
 
 def lint():
-    install_test_dependencies()
+    install()
     local('flake8 .')
 
 
 def test():
+    install()
     lint()
     local('python -m multitest discover -v -t . -s test')
 
 
 def coverage():
-    install_test_dependencies()
+    install()
     local('coverage run -m multitest discover -t . -s test 2> /dev/null')
     local('coverage combine')
     local('coverage report -m')
 
 
-def debug(runtime='local'):
+def debug():
     """Starts up debug server. Run with `debug:docker` to run with docker"""
-    install_test_dependencies()
-    with shell_env(GAE_LOCAL_VM_RUNTIME='1'):
-        local('''
-        dev_appserver.py --host 0.0.0.0 --log_level debug {MODULES}'''.format(**app_config))
+    install()
+    local('dev_appserver.py --host 0.0.0.0 --log_level debug app.yaml')
 
 
-if environ.get('CI'):
-    def secrets():
-        """Validates and sets backend secrets"""
-        with open(app_config['CONFIG'], 'r+') as backend_config:
-            text = backend_config.read()
-            for arg in BACKEND_ARGS:
-                if arg not in text:
-                    raise Exception('{} not found in {}'.format(arg, app_config['CONFIG']))
-                text = re.sub(r'{0} = \'.*\''.format(arg),
-                              '{0} = \'{1}\''.format(arg, app_config[arg]),
-                              text)
-            _overwrite(backend_config, text)
-        local('flake8 {CONFIG} --max-line-length=1000'.format(**app_config))
-
-    def production():
-        """Configures deploy for production"""
-        app_config['APPLICATION'] = 'your-app'  # TODO better app detection
+def deploy():
+    """Deploys application"""
+    config = {}
+    for arg in ALL_ARGS:
+        config[arg] = environ[arg]
+    with open('config.py', 'r+') as config_file:
+        text = config_file.read()
         for arg in ALL_ARGS:
-            app_config[arg] = environ[arg]
-
-    def staging():
-        """Configures deploy for staging"""
-        app_config['APPLICATION'] = 'your-app-staging'  # TODO better app detection
-        app_config['MODULES'] = 'app.yaml discovery.yaml platform.yaml sender.yaml video.yaml'
-        for arg in COMMON_ARGS:
-            app_config[arg] = environ[arg]
-        for arg in APPLICATION_ARGS:
-            app_config[arg] = environ['STAGING_{}'.format(arg)]
-        # minimize instance count for staging
-        for module in app_config['MODULES'].split(' '):
-            with open(module, 'r+') as module_file:
-                text = module_file.read()
-                text = re.sub(r'min_idle_instances: .*', 'min_idle_instances: 0', text)
-                text = re.sub(r'max_idle_instances: .*', 'max_idle_instances: 1', text)
-                text = re.sub(r'min_num_instances: .*', 'min_num_instances: 1', text)
-                _overwrite(module_file, text)
-
-    def validate_env():
-        """Validates required files and environment variables"""
-        production()
-        staging()
-        secrets()
-
-    def deploy_application():
-        """Deploys application with given config"""
-        if 'APPLICATION' not in app_config:
-            raise Exception('Deploy environment not specified. Use `fab staging deploy` or `fab production deploy`.')
-        secrets()
-        for module in app_config['MODULES'].split(' '):
-            with open(module, 'r+') as module_file:
-                text = module_file.read()
-                text = re.sub(r'application: .*', 'application: {}'.format(app_config['APPLICATION']), text)
-                _overwrite(module_file, text)
-
-        local('appcfg.py update {} --oauth2_refresh_token={}'
-              .format(app_config['MODULES'], app_config['REFRESH_TOKEN']))
-
-    def deploy_staging():
-        """Alias for `fab staging deploy_application`"""
-        staging()
-        deploy_application()
-
-    def deploy():
-        """Alias for `fab production deploy_application`"""
-        production()
-        deploy_application()
+            if arg not in text:
+                raise Exception('{} not found in config file'.format(arg))
+            print arg, config[arg], text
+            text = re.sub(r'{0} = .*'.format(arg), '{0} = \'{1}\''.format(arg, config[arg]), text)
+        config_file.seek(0)
+        config_file.write(text)
+        config_file.truncate()
+    lint()
+    try:
+        refresh_token = environ[REFRESH_TOKEN_NAME]
+    except KeyError:
+        with hide('running'):
+            local('echo {} was not found in the environment'.format(REFRESH_TOKEN_NAME))
+            local('echo Retrieving refresh token')
+            local('appcfg.py list_versions .', capture=True)
+            with open('{}/.appcfg_oauth2_tokens'.format(environ['HOME'])) as token_file:
+                token_text = token_file.read()
+            refresh_token = json.loads(token_text)['refresh_token']
+            local('echo Your refresh token is: {}.')
+            local('echo Please have it in the environment using: '.format(refresh_token))
+            local('echo export {}={}'.format(REFRESH_TOKEN_NAME, refresh_token))
+    local('appcfg.py update . --oauth2_refresh_token {}'.format(refresh_token))
+    local('appcfg.py update_queues . --oauth2_refresh_token {}'.format(refresh_token))
+    local('appcfg.py update_indexes . --oauth2_refresh_token {}'.format(refresh_token))
+    local('appcfg.py update_cron . --oauth2_refresh_token {}'.format(refresh_token))
