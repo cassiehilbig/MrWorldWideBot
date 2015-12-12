9e04f21bb27c2ed8af186998c58421da80a98bbf
Aleem Haji <hajial@gmail.com>
2015-12-12 00:19:47 -0500

    Removed the const file, because it was unused.

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
 
     # AppEngine supports queueing up 100 tasks at once
     MAX_TASKQUEUE_BATCH_SIZE = 100
+    # Taskqueues can't take more than 100KB, this is a little smaller to allow for headers
+    TASKQUEUE_MAX_LENGTH = 90 * 1024
 
     BOT_USERNAME = os.environ['BOT_USERNAME']
     BOT_API_KEY = os.environ['BOT_API_KEY']
