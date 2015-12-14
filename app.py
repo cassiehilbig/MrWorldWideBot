978d5c8f2097b3591e301d59704395645eeb54c8
tcooc <tcooc@users.noreply.github.com>
2015-12-14 14:38:04 -0500

    Simplify api paths.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from importlib import import_module
-import os
+from os import listdir, path
 import sys
 import json
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '.'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'xlib'))
+sys.path.insert(0, path.join(path.dirname(__file__), '.'))
+sys.path.insert(0, path.join(path.dirname(__file__), 'xlib'))
 
 from flask import Flask, request
 
@@ -19,15 +19,8 @@ def parse_data():
         except ValueError:
             pass
 
-
-module = os.environ.get('CURRENT_MODULE_ID', 'default')
-
-path = ('{}/api/{}'.format(os.path.dirname(os.path.realpath(__file__)), module))
-routes = []
-
-for file_name in os.listdir(path):
+for file_name in listdir('{}/api'.format(path.dirname(path.realpath(__file__)))):
     if not file_name.startswith('.') and file_name.endswith('.py') and file_name != '__init__.py':
-        api_name = file_name[:-3]
-        import_module('api.{}.{}'.format(module, api_name))
+        import_module('api.{}'.format(file_name[:-3]))
 
 __all__ = ['app']
