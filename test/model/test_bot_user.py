b7d3c32754c501117294797d563056b5e22d9ccd
Aleem Haji <hajial@gmail.com>
2015-12-15 15:58:00 -0500

    Fix up issue with __abc__ with app engine models.

--- test/model/test_bot_user.py
+++ test/model/test_bot_user.py
@@ -1,8 +1,23 @@
+from google.appengine.ext import ndb
+
 from test.test_base import TestBase
 from model import BotUser
 
 
 class TestBotUser(TestBase):
+    def test_creation(self):
+        self.assertEqual(BotUser().key, None)
+        self.assertEqual(BotUser(id='aleem').key.id(), 'aleem')
+        self.assertEqual(BotUser(id='aleem').id, 'aleem')
+        self.assertEqual(BotUser(id='__aleem__').key.id(), '@__aleem__')
+        self.assertEqual(BotUser(id='__aleem__').id, '__aleem__')
+
+    def test_get_by_id(self):
+        ndb.put_multi([BotUser(id='aleem'), BotUser(id='__aleem__')])
+        users = [BotUser.get_by_id('aleem'), BotUser.get_by_id('__aleem__')]
+        self.assertEqual([user.id for user in users], ['aleem', '__aleem__'])
+        self.assertEqual([user.key.id() for user in users], ['aleem', '@__aleem__'])
+
     def test_states_defaults(self):
         user = BotUser()
         with self.assertRaises(AttributeError):
@@ -47,12 +62,6 @@ class TestBotUser(TestBase):
         user.clear_current_state_data()
         self.assertEqual(user.current_state_data(), {})
 
-    def test_clear_current_state_data_no_states(self):
-        user = BotUser()
-        self.assertRaises(Exception, user.clear_current_state_data)
-
-    def test_clear_current_state_data_no_data(self):
-        user = BotUser(states=['some_state'], state_data={'foo': {'bar': 'baz'}})
-        user.clear_current_state_data()
-        self.assertEqual(user.state_data, {'foo': {'bar': 'baz'}})
-        self.assertEqual(user.current_state_data(), {})
+    def test_generate_id(self):
+        self.assertEqual(BotUser._generate_id('aleem'), 'aleem')
+        self.assertEqual(BotUser._generate_id('__aleem__'), '@__aleem__')
