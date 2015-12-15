7ef8272418d7eb99299156c91cc0ef41290846bc
remvst <remvst@gmail.com>
2015-12-15 10:30:15 -0500

    Drop folder

--- xlib/kik/state_machine/__init__.py
+++
@@ -1,9 +0,0 @@
-from state import State
-from keyword_state import KeywordState, keyword_response
-from confirmation_state import ConfirmationState
-from state_machine import StateMachine
-from transition import Transition, PopTransition, StackTransition, LambdaTransition
-from persistence_strategy import PersistenceStrategy
-
-__all__ = ['State', 'StateMachine', 'Transition', 'PopTransition', 'StackTransition', 'LambdaTransition',
-           'KeywordState', 'keyword_response', 'ConfirmationState', 'PersistenceStrategy']
