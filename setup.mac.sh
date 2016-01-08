0c46906da4733ffc26b59e26f9984eb3f0b08d0e
Aleem Haji <hajial@gmail.com>
2016-01-08 16:34:39 -0500

    Simplify installation procedure.

--- setup.mac.sh
+++ setup.mac.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if ! type "brew" > /dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
 
 brew install git python app-engine-python ngrok
+
+# In case something was already installed, updgrade new version.
+brew upgrade
+
 pip install --upgrade --quiet --requirement requirements.txt
