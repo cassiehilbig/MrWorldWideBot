7fe34bb5b69669d49383d68d1c46155b04bb9a4e
Aleem Haji <hajial@gmail.com>
2016-01-11 11:03:49 -0500

    upgrade before install.

--- setup.mac.sh
+++ setup.mac.sh
@@ -2,13 +2,12 @@
 
 if ! type "brew" > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    brew update
 fi
 
-brew install git python app-engine-python ngrok
-
-# In case something was already installed, updgrade new version.
+# Make sure we're using the latest brew, then upgrade all existing items.
+brew update
 brew upgrade
+
+brew install git python app-engine-python ngrok
 
 pip install --upgrade --quiet --requirement requirements.txt
