50330ed7b7fcb672022888defcedc7882c283c0d
Aleem Haji <hajial@gmail.com>
2016-01-11 14:02:17 -0500

    Moved the tap higher.

--- setup.mac.sh
+++ setup.mac.sh
@@ -4,11 +4,11 @@ if ! type "brew" > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew tap caskroom/cask
+
 # Make sure we're using the latest brew, then upgrade all existing items.
 brew update
 brew upgrade
-
-brew tap caskroom/cask
 
 brew install git python app-engine-python
 brew cask install ngrok
