e8926014032f0b8cc3457ea3d9df79d927b0e167
Aleem Haji <hajial@gmail.com>
2016-01-11 14:58:15 -0500

    Updated so that deps are upraded explicitly.

--- setup.mac.sh
+++ setup.mac.sh
@@ -4,13 +4,19 @@ if ! type "brew" > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+dependencies=(
+    git 
+    python 
+    app-engine-python
+)
+
 brew tap caskroom/cask
 
 # Make sure we're using the latest brew, then upgrade all existing items.
 brew update
-brew upgrade
+brew upgrade ${dependencies[@]} 2> /dev/null
 
-brew install git python app-engine-python
+brew install ${dependencies[@]}
 brew cask install ngrok
 
 pip install --upgrade --quiet --requirement requirements.txt
