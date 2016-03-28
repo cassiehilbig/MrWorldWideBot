f7db3ff0377dbb1ab8456bd43567e6d2596f62e6
Kai-Yanna Meighu <kai-yanna.meighu@kik.com>
2016-03-28 16:51:12 -0400

    Add sample app

---
+++ lib/states/color/__init__.py
@@ -0,0 +1,3 @@
+from choose_favorite_color_flow import ChooseColorState, ConfirmColorState
+
+__all__ = ['ChooseColorState', 'ConfirmColorState']
---
+++ lib/states/default_state.py
@@ -0,0 +1,22 @@
+from kik.api import get_user_profile
+from kik.messages import make_text_message
+from kik.state_machine import State, Transition
+
+from lib.states.state_types import StateTypes
+from config import Config
+
+
+class DefaultStateStrings(object):
+    WELCOME_MESSAGE = 'Welcome, {first_name}!'
+
+
+class DefaultState(State):
+
+    @staticmethod
+    def type():
+        return StateTypes.DEFAULT
+
+    def on_message(self, message):
+        profile = get_user_profile(message['from'], bot_name=Config.BOT_USERNAME, bot_api_key=Config.BOT_API_KEY)
+        m = DefaultStateStrings.WELCOME_MESSAGE.format(first_name=profile['firstName'])
+        return Transition([make_text_message(self.user.id, m)], StateTypes.MENU)
